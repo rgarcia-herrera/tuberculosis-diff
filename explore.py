b03fb424f517c62c6db259f33e1ff6e547fe31c3
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-13 17:35:26 -0500

    test normalized

--- explore.py
+++ explore.py
@@ -3,30 +3,21 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 from itertools import combinations
 
-raws=['/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP1_24H_L1.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP1_24H_L2.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP1_48H_L1.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP1_48H_L2.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP2_24H_L1.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP2_24H_L2.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP2_48H_L1.txt',
-      '/srv/home/rgarcia/microarreglos/nrp1_nrp2/raw/gRaw_NRP2_48H_L2.txt']
-
-aaloess=[
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP1_24H_L1.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP1_24H_L2.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP1_48H_L1.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP1_48H_L2.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP2_24H_L1.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP2_24H_L2.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP2_48H_L1.txt',
-    '/srv/home/rgarcia/microarreglos/nrp1_nrp2/out/ng_NRP2_48H_L2.txt']
-
+normalized=[
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_24h_l1.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_24h_l2.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_48h_l1.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_48h_l2.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_24h_l1.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_24h_l2.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_48h_l1.txt',
+    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_48h_l2.txt',
+]
 
 A = {}
 probes = set()
 
-for path in aaloess:
+for path in normalized:
     with open(path) as f:
         r = csv.reader(f, delimiter='\t')
         sample = r.next().pop()
@@ -43,7 +34,7 @@ for pair in combinations(A.keys(), 2):
     x = [A[pair[0]][probe] for probe in probes]
     y = [A[pair[1]][probe] for probe in probes]
     plt.scatter(x, y)
-    plt.title("raw intensities")
-    plt.xlabel(pair[0].replace("gRaw_", "").replace("_", " ").replace(".txt", ""))
-    plt.ylabel(pair[1].replace("gRaw_", "").replace("_", " ").replace(".txt", ""))
-    plt.savefig("%s_%s.png" % pair)
+    plt.title("intensities")
+    plt.xlabel(pair[0])
+    plt.ylabel(pair[1])
+    plt.savefig("plots/%s_%s.png" % pair)
