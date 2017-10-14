ad264ece09b35d886279cb44542d55b241dc1b5b
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-14 14:26:31 -0500

    contraste de macrofago

--- design.py
+++ design.py
@@ -1,18 +1,18 @@
-nrp1_24 = {'path': 'data/nrp1_nrp2/norm/nrp1_24h_tb.tsv',
+nrp1_24_tb = {'path': 'data/nrp1_nrp2/norm/nrp1_24h_tb.tsv',
            'title': 'NRP1 24h'}
-nrp1_48 = {'path': 'data/nrp1_nrp2/norm/nrp1_48h_tb.tsv',
+nrp1_48_tb = {'path': 'data/nrp1_nrp2/norm/nrp1_48h_tb.tsv',
            'title': 'NRP1 48h'}
-nrp2_24 = {'path': 'data/nrp1_nrp2/norm/nrp2_24h_tb.tsv',
+nrp2_24_tb = {'path': 'data/nrp1_nrp2/norm/nrp2_24h_tb.tsv',
            'title': 'NRP2 24h'}
-nrp2_48 = {'path': 'data/nrp1_nrp2/norm/nrp2_48h_tb.tsv',
+nrp2_48_tb = {'path': 'data/nrp1_nrp2/norm/nrp2_48h_tb.tsv',
            'title': 'NRP2 48h'}
 
 contrasts_tb = {
-    'TB NRP1 24h vs. NRP1 48h': [nrp1_24, nrp1_48],
-    'TB NRP2 24h vs. NRP2 48h': [nrp2_24, nrp2_48],
+    'TB NRP1 24h vs. NRP1 48h': [nrp1_24_tb, nrp1_48_tb],
+    'TB NRP2 24h vs. NRP2 48h': [nrp2_24_tb, nrp2_48_tb],
 
-    'TB NRP1 24h vs. NRP2 24h': [nrp1_24, nrp2_24],
-    'TB NRP1 48h vs. NRP2 48h': [nrp1_48, nrp2_48],
+    'TB NRP1 24h vs. NRP2 24h': [nrp1_24_tb, nrp2_24_tb],
+    'TB NRP1 48h vs. NRP2 48h': [nrp1_48_tb, nrp2_48_tb],
 
 # pending inter-array normalization
 #    'NPR1 24h vs. LOG 24h': [],
@@ -20,4 +20,22 @@ contrasts_tb = {
 
 #    'LOG 24h vs. LOG 48h': [],
 
+}
+
+
+nrp1_24_mo = {'path': 'data/nrp1_nrp2/norm/nrp1_24h_macrophage.tsv',
+              'title': 'NRP1 24h'}
+nrp1_48_mo = {'path': 'data/nrp1_nrp2/norm/nrp1_48h_macrophage.tsv',
+              'title': 'NRP1 48h'}
+nrp2_24_mo = {'path': 'data/nrp1_nrp2/norm/nrp2_24h_macrophage.tsv',
+              'title': 'NRP2 24h'}
+nrp2_48_mo = {'path': 'data/nrp1_nrp2/norm/nrp2_48h_macrophage.tsv',
+              'title': 'NRP2 48h'}
+
+contrasts_macrophage = {
+    'Macrophage NRP1 24h vs. NRP1 48h': [nrp1_24_mo, nrp1_48_mo],
+    'Macrophage NRP2 24h vs. NRP2 48h': [nrp2_24_mo, nrp2_48_mo],
+
+    'Macrophage NRP1 24h vs. NRP2 24h': [nrp1_24_mo, nrp2_24_mo],
+    'Macrophage NRP1 48h vs. NRP2 48h': [nrp1_48_mo, nrp2_48_mo],
 }
--- scatterplot.py
+++ scatterplot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from slugify import slugify
 
-from design import contrasts_tb
+from design import contrasts_tb, contrasts_macrophage
 
 
 def scatter_plot(title, pair, color='red'):
@@ -14,7 +14,7 @@ def scatter_plot(title, pair, color='red'):
     x = np.genfromtxt(pair[0]['path'], delimiter=',', usecols=(1,))
     y = np.genfromtxt(pair[1]['path'], delimiter=',', usecols=(1,))
 
-    plt.scatter(x, y, alpha=0.11, color=red, marker='.')
+    plt.scatter(x, y, alpha=0.11, color=color, marker='.')
     plt.title(title)
     plt.xlabel(pair[0]['title'])
     plt.ylabel(pair[1]['title'])
@@ -23,3 +23,7 @@ def scatter_plot(title, pair, color='red'):
 
 for title in contrasts_tb:
     scatter_plot(title, contrasts_tb[title])
+
+
+for title in contrasts_macrophage:
+    scatter_plot(title, contrasts_macrophage[title], color='blue')
