88c406559a3ba43be76fba7c1ed6d841fef7da00
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-14 10:46:32 -0500

    contrastes TB

---
+++ design.py
@@ -0,0 +1,12 @@
+contrasts_tb = {
+
+    'NPR1 24h vs. NPR1 48h': [],
+    'NPR2 24h vs. NPR2 48h': [],
+    'LOG 24h vs. LOG 48h': [],
+
+    'NPR1 24h vs. NPR2 24h': [],
+    'NPR1 24h vs. LOG 24h': [],
+
+    'NPR1 48h vs. LOG 48h': [],
+    'NPR1 48h vs. NPR2 48h': [],
+}
