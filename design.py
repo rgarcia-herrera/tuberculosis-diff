79e6b2271b722827cad300eddafcac43aeffd1d5
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-14 11:28:32 -0500

    quiza las rutas

--- design.py
+++ design.py
@@ -1,12 +1,19 @@
+nrp1_24 = 'data/nrp1_nrp2/norm/nrp1_24h.txt'
+nrp1_48 = 'data/nrp1_nrp2/norm/nrp1_48h.txt'
+nrp2_24 = 'data/nrp1_nrp2/norm/nrp2_24h.txt'
+nrp2_48 = 'data/nrp1_nrp2/norm/nrp2_48h.txt'
+
 contrasts_tb = {
+    'NRP1 24h vs. NRP1 48h': [nrp1_24, nrp1_48],
+    'NRP2 24h vs. NRP2 48h': [nrp2_24, nrp2_48],
 
-    'NPR1 24h vs. NPR1 48h': [],
-    'NPR2 24h vs. NPR2 48h': [],
-    'LOG 24h vs. LOG 48h': [],
+    'NRP1 24h vs. NRP2 24h': [nrp1_24, nrp2_24],
+    'NRP1 48h vs. NRP2 48h': [nrp1_48, nrp2_48],
 
-    'NPR1 24h vs. NPR2 24h': [],
-    'NPR1 24h vs. LOG 24h': [],
+# pending inter-array normalization
+#    'NPR1 24h vs. LOG 24h': [],
+#    'NPR1 48h vs. LOG 48h': [],
 
-    'NPR1 48h vs. LOG 48h': [],
-    'NPR1 48h vs. NPR2 48h': [],
+#    'LOG 24h vs. LOG 48h': [],
+
 }
