50d098a14ae0e2e4e61b4be0252611e80ab4a61e
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-14 14:18:18 -0500

    unos contrastes

--- design.py
+++ design.py
@@ -1,14 +1,18 @@
-nrp1_24 = 'data/nrp1_nrp2/norm/nrp1_24h.txt'
-nrp1_48 = 'data/nrp1_nrp2/norm/nrp1_48h.txt'
-nrp2_24 = 'data/nrp1_nrp2/norm/nrp2_24h.txt'
-nrp2_48 = 'data/nrp1_nrp2/norm/nrp2_48h.txt'
+nrp1_24 = {'path': 'data/nrp1_nrp2/norm/nrp1_24h_tb.tsv',
+           'title': 'NRP1 24h'}
+nrp1_48 = {'path': 'data/nrp1_nrp2/norm/nrp1_48h_tb.tsv',
+           'title': 'NRP1 48h'}
+nrp2_24 = {'path': 'data/nrp1_nrp2/norm/nrp2_24h_tb.tsv',
+           'title': 'NRP2 24h'}
+nrp2_48 = {'path': 'data/nrp1_nrp2/norm/nrp2_48h_tb.tsv',
+           'title': 'NRP2 48h'}
 
 contrasts_tb = {
-    'NRP1 24h vs. NRP1 48h': [nrp1_24, nrp1_48],
-    'NRP2 24h vs. NRP2 48h': [nrp2_24, nrp2_48],
+    'TB NRP1 24h vs. NRP1 48h': [nrp1_24, nrp1_48],
+    'TB NRP2 24h vs. NRP2 48h': [nrp2_24, nrp2_48],
 
-    'NRP1 24h vs. NRP2 24h': [nrp1_24, nrp2_24],
-    'NRP1 48h vs. NRP2 48h': [nrp1_48, nrp2_48],
+    'TB NRP1 24h vs. NRP2 24h': [nrp1_24, nrp2_24],
+    'TB NRP1 48h vs. NRP2 48h': [nrp1_48, nrp2_48],
 
 # pending inter-array normalization
 #    'NPR1 24h vs. LOG 24h': [],
--- scatterplot.py
+++ scatterplot.py
@@ -1,40 +1,25 @@
-import csv
 from pprint import pprint
+import matplotlib
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
-from itertools import combinations
+import numpy as np
+from slugify import slugify
 
-normalized=[
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_24h_l1.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_24h_l2.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_48h_l1.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp1_48h_l2.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_24h_l1.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_24h_l2.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_48h_l1.txt',
-    '/srv/home/rgarcia/tuberculosis-nrp/data/nrp1_nrp2/norm/ng_nrp2_48h_l2.txt',
-]
-
-A = {}
-probes = set()
-
-for path in normalized:
-    with open(path) as f:
-        r = csv.reader(f, delimiter='\t')
-        sample = r.next().pop()
-        A[sample] = {}
-        for row in r:
-            (probe, intensity) = row
-            probes.add(probe)
-            A[sample][probe] = float(intensity)
-
-probes = list(probes)
+from design import contrasts_tb
 
 
-for pair in combinations(A.keys(), 2):
-    x = [A[pair[0]][probe] for probe in probes]
-    y = [A[pair[1]][probe] for probe in probes]
-    plt.scatter(x, y)
-    plt.title("intensities")
-    plt.xlabel(pair[0])
-    plt.ylabel(pair[1])
-    plt.savefig("plots/%s_%s.png" % pair)
+def scatter_plot(title, pair, color='red'):
+
+    x = np.genfromtxt(pair[0]['path'], delimiter=',', usecols=(1,))
+    y = np.genfromtxt(pair[1]['path'], delimiter=',', usecols=(1,))
+
+    plt.scatter(x, y, alpha=0.11, color=red, marker='.')
+    plt.title(title)
+    plt.xlabel(pair[0]['title'])
+    plt.ylabel(pair[1]['title'])
+    plt.savefig("plots/%s" % slugify(title))
+    plt.cla()
+
+for title in contrasts_tb:
+    scatter_plot(title, contrasts_tb[title])
