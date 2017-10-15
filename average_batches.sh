a02d3bdc93afec5821116e3fb55dfea54da13d32
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-14 23:28:04 -0500

    reemplaza tab por espacio en ctls

--- average_batches.sh
+++ average_batches.sh
@@ -10,16 +10,28 @@ cd ../../../
 cd data/log_ctrl_lavado0/norm/
 paste ng_log_24h_l1.txt ng_log_24h_l2.txt | awk '{print $1,($2+$4)/2}' > log_24h.txt
 paste ng_log_48h_l1.txt ng_log_48h_l2.txt ng_log_48h_l3.txt | awk '{print $1,($2+$4+$6)/3}' > log_48h.txt
+# s/\t/ /
+awk '{print $1,$2}' ng_ctrl_4h.txt > ctrl_4h.txt
+awk '{print $1,$2}' ng_ctrl_24h.txt > ctrl_24h.txt
+awk '{print $1,$2}' ng_ctrl_48h.txt > ctrl_48h.txt
 cd ../../../
 
 
 cd data/log_ctrl_lavado1/norm/
 paste ng_log_24h_l1.txt ng_log_24h_l2.txt | awk '{print $1,($2+$4)/2}' > log_24h.txt
 paste ng_log_48h_l1.txt ng_log_48h_l2.txt ng_log_48h_l3.txt | awk '{print $1,($2+$4+$6)/3}' > log_48h.txt
+# s/\t/ /
+awk '{print $1,$2}' ng_ctrl_4h.txt > ctrl_4h.txt
+awk '{print $1,$2}' ng_ctrl_24h.txt > ctrl_24h.txt
+awk '{print $1,$2}' ng_ctrl_48h.txt > ctrl_48h.txt
 cd ../../../
 
 
 cd data/log_ctrl_lavado2/norm/
 paste ng_log_24h_l1.txt ng_log_24h_l2.txt | awk '{print $1,($2+$4)/2}' > log_24h.txt
 paste ng_log_48h_l1.txt ng_log_48h_l2.txt ng_log_48h_l3.txt | awk '{print $1,($2+$4+$6)/3}' > log_48h.txt
+# s/\t/ /
+awk '{print $1,$2}' ng_ctrl_4h.txt > ctrl_4h.txt
+awk '{print $1,$2}' ng_ctrl_24h.txt > ctrl_24h.txt
+awk '{print $1,$2}' ng_ctrl_48h.txt > ctrl_48h.txt
 cd ../../../
