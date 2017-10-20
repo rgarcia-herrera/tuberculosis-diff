7500e224b7d4fa363a135475b28052a6d54e9010
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-20 16:54:03 -0500

    separadores

--- functional_categories/__init__.py
+++ functional_categories/__init__.py
@@ -3,14 +3,19 @@ from os import path
 
 
 ann={}
+
 for n in [0, 1, 2, 3, 5, 6, 7, 8, 9, 10]:
     with open(path.join(path.dirname(path.realpath(__file__)),
                         "%s.txt" % n), 'r') as f:
         r = csv.reader(f, delimiter="\t")
+        ann_header = r.next() + ['FC',]
+
         for row in r:
             synonyms = [syn.replace("<I>", "").replace("</I>", "")
                         for syn in row[1].split(', ')]
-            row[1] = ";".join(synonyms)
+            row[1] = ",".join(synonyms)
+
+            row.append(n)
 
             ann[row[0]] = row
             for syn in synonyms:
